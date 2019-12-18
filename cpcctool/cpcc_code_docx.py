f41e75a22f0e3678e8bb770c7b8d82d99fc00782
Leon <l2m2lq@gmail.com>
2019-12-18 08:26:47 +0800

    upload.

--- cpcctool/cpcc_code_docx.py
+++ cpcctool/cpcc_code_docx.py
@@ -13,7 +13,6 @@ from .source_tie import tie
 from .txt2docx import txt2docx
 
 def first_40_pages(docx_file, dst_file):
-  print(dst_file)
   app = win32.DispatchEx("Word.Application")
   app.Visible = 0
   app.DisplayAlerts = 0
@@ -21,16 +20,35 @@ def first_40_pages(docx_file, dst_file):
   try:
     doc = app.ActiveDocument
     doc.Repaginate()
-    # page_count = doc.ComputeStatistics(2)
-    app.Selection.GoTo(1, 1, 2)
-    doc.Bookmarks("\\Page").Range.Delete()
+    page_count = doc.ComputeStatistics(2)
+    app.Selection.GoTo(1, 1, 40)
+    r = doc.Bookmarks("\\Page").Range
+    app.Selection.GoTo(1, 1, page_count)
+    r.End = doc.Bookmarks("\\Page").Range.End
+    r.Delete()
     doc.SaveAs(dst_file, 16)
     doc.Close(SaveChanges=0)
   finally:
     app.Quit()
 
 def last_40_pages(docx_file, dst_file):
-  pass
+  app = win32.DispatchEx("Word.Application")
+  app.Visible = 0
+  app.DisplayAlerts = 0
+  app.Documents.Open(docx_file)
+  try:
+    doc = app.ActiveDocument
+    doc.Repaginate()
+    page_count = doc.ComputeStatistics(2)
+    app.Selection.GoTo(1, 1, 1)
+    r = doc.Bookmarks("\\Page").Range
+    app.Selection.GoTo(1, 1, page_count - 40)
+    r.End = doc.Bookmarks("\\Page").Range.End
+    r.Delete()
+    doc.SaveAs(dst_file, 16)
+    doc.Close(SaveChanges=0)
+  finally:
+    app.Quit()
 
 def gen_code_docx(src_dirs, dst_file):
   tmp_txt_file = tempfile.gettempdir() + os.sep + str(uuid.uuid4())
